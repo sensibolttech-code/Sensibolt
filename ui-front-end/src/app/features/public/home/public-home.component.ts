0a27f0915cccb80e1f5cc9572e9cb310dbbe66a8
Jay Prakash Kumar <jay.kasyap@live.com>
2026-04-02 21:52:36 +0530

    semi-cond-section

--- ui-front-end/src/app/features/public/home/public-home.component.ts
+++ ui-front-end/src/app/features/public/home/public-home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { BusinessSegmentsComponent } from './business-segments.component';
+import { SemiconductorServicesComponent } from './semiconductor-services.component';
 
 @Component({
     selector: 'app-public-home',
     standalone: true,
-    imports: [CommonModule, RouterLink],
+    imports: [CommonModule, RouterLink, BusinessSegmentsComponent, SemiconductorServicesComponent],
     templateUrl: './public-home.component.html',
     styleUrl: './public-home.component.scss'
 })
