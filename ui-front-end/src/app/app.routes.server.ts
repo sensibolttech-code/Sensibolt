688e84c06a3b21f09ef2bcb96ffcbc01b2edbb5d
sensibolttech-code <sensibolttech@gmail.com>
2026-04-10 21:13:25 +0530

    Render error fixed

--- ui-front-end/src/app/app.routes.server.ts
+++ ui-front-end/src/app/app.routes.server.ts
@@ -2,6 +2,21 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'public/services/:category',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      const categories = [
+        'digital-workplace',
+        'digital-infrastructure',
+        'managed-it',
+        'audit-compliance',
+        'data-analytics',
+        'smt-manufacturing'
+      ];
+      return categories.map(category => ({ category }));
+    }
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
